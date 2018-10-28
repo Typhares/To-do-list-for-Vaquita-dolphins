3ddd0ccb6e1554de02e7db168938ee1c3e0418f7
Elizabeth Hall <elizabethhall205@gmail.com>
2018-10-27 23:01:55 -0400

    first push

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-'strict'
+'use strict';
+
+
+$("li").click(function(){
+    if li is gray
+        turn it back
+    else if 
+        turn back
+    $(this).css({
+        color: "gray",
+        textDecoration: "line-through"
+    })
+});
